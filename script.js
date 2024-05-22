dc23ac73a70db350583ead616766870f81c15615
Darion Valdez <darionvldz@gmail.com>
2024-05-22 19:20:01 +0200

    refactor code

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const nextButton = document.querySelector("#next-button");
-const prevButton = document.querySelector("#prev-button");
 const IMAGE_WIDTH_IN_PIXELS = 300;
 
 let imageIndex = 0;
-let distanceToMoveInPixels = IMAGE_WIDTH_IN_PIXELS;
+let distanceToMoveInPixels = 0;
 
 const images = [
   "images/01.jpg",
@@ -12,37 +10,33 @@ const images = [
   "images/04.jpg",
   "images/05.jpg",
   "images/06.jpg",
-  "images/07.jpg",
 ];
 
-const imageElements = document.querySelectorAll(".image");
+(function initializeGallery() {
+  const nextButton = document.querySelector("#next-button");
+  const prevButton = document.querySelector("#prev-button");
 
-nextButton.addEventListener("click", () => {
-  distanceToMoveInPixels -= IMAGE_WIDTH_IN_PIXELS;
-  nextImage(distanceToMoveInPixels);
-  deselectImages();
-  selectImage(getNextImage());
-});
+  nextButton.addEventListener("click", nextImage);
+  prevButton.addEventListener("click", previousImage);
 
-prevButton.addEventListener("click", () => {
-  distanceToMoveInPixels += IMAGE_WIDTH_IN_PIXELS;
-  previousImage(distanceToMoveInPixels);
-  deselectImages();
-  selectImage(getPreviousImage());
-});
+  for (const image of images) {
+    addImage(createImage(image));
+  }
+  centerFirstImage();
+  selectImage(getImages()[0]);
+})();
 
 function centerFirstImage() {
+  // Bildspelet är centrerat med justify-content: center
+  // Om bildspelet har tex 7 bilder hamnar bild 4 i mitten.
+  // Vi behöver hoppa 3 steg åt vänster
   for (let i = 1; i < images.length; i += 2) {
-    previousImage(distanceToMoveInPixels);
-    distanceToMoveInPixels += IMAGE_WIDTH_IN_PIXELS;
-    console.log("test");
+    moveLeft();
   }
-  distanceToMoveInPixels -= IMAGE_WIDTH_IN_PIXELS;
-
+  // Om jämnt antal bilder i bildspelet hamnar första bilden halvt åt sidan.
+  // moveHalfStep åtgärdar det.
   if (images.length % 2 == 0) {
-    nextImage(distanceToMoveInPixels - IMAGE_WIDTH_IN_PIXELS / 2);
-    distanceToMoveInPixels -= IMAGE_WIDTH_IN_PIXELS / 2;
-    console.log("te");
+    moveLeftHalfstep();
   }
 }
 
@@ -53,28 +47,51 @@ function getNextImage() {
 }
 
 function getPreviousImage() {
-  const images = document.querySelectorAll(".image");
+  const images = getImages();
   imageIndex--;
   return images[imageIndex];
 }
 
-function nextImage(distance) {
-  const images = document.querySelectorAll(".image");
-  for (const image of images) {
-    image.style.transform = `translateX(${distance}px)`;
+function getImages() {
+  return document.querySelectorAll(".image");
+}
+
+function nextImage() {
+  if (imageIndex === images.length - 1) return;
+  moveRight();
+  deselectImages();
+  selectImage(getNextImage());
+}
+
+function previousImage() {
+  if (imageIndex === 0) return;
+  moveLeft();
+  deselectImages();
+  selectImage(getPreviousImage());
+}
+
+function moveRight() {
+  distanceToMoveInPixels -= IMAGE_WIDTH_IN_PIXELS;
+  const imageElements = getImages();
+  for (const image of imageElements) {
+    image.style.transform = `translateX(${distanceToMoveInPixels}px)`;
   }
 }
 
-function previousImage(distance) {
-  const images = document.querySelectorAll(".image");
-  for (const image of images) {
-    image.style.transform = `translateX(${distance}px)`;
+function moveLeft() {
+  distanceToMoveInPixels += IMAGE_WIDTH_IN_PIXELS;
+  const imageElements = getImages();
+  for (const image of imageElements) {
+    image.style.transform = `translateX(${distanceToMoveInPixels}px)`;
   }
 }
 
-function renderImage(imageToRender) {
-  const image = document.querySelector("#image");
-  image.src = imageToRender;
+function moveLeftHalfstep() {
+  distanceToMoveInPixels -= IMAGE_WIDTH_IN_PIXELS / 2;
+  const imageElements = getImages();
+  for (const image of imageElements) {
+    image.style.transform = `translateX(${distanceToMoveInPixels}px)`;
+  }
 }
 
 function selectImage(image) {
@@ -88,5 +105,15 @@ function deselectImages() {
   }
 }
 
-centerFirstImage();
-selectImage(imageElements[0]);
+function createImage(src) {
+  const newImage = document.createElement("img");
+  newImage.src = src;
+  newImage.classList.add("image");
+
+  return newImage;
+}
+
+function addImage(image) {
+  const galleryContainer = document.querySelector(".gallery-container");
+  galleryContainer.appendChild(image);
+}
